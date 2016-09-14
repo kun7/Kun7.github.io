fa1846e10e8863ce82cabeddbabf99fbe2311f87
kun7 <lanang7104@gmail.com>
2016-09-14 21:45:37 +0700

    dos

---
+++ phase0-challenge/konversiWaktu.js
@@ -0,0 +1,10 @@
+var konversiWaktu = function (minuto){//fungsi lgsg membagi antara jam dan menit
+  if (minuto){ var minuto = {}; var jam = (); var menit = (minuto - 60);
+    return jam + menit
+    console.log(jam ":" menit)
+
+  }
+}
+konversiWaktu(73);
+// konversiWaktu(89);
+// +" "+ menit+ "menit"var menit = number -60
